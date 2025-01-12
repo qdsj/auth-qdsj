6af4f669ef0b0e86cf588db3833e1c96b40c6c05
zenos <zenos.chen@foxmail.com>
2025-01-12 11:34:26 +0800

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,29 +3,29 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './auth/entities/User.entity';
-import { Auth } from './auth/entities/auth.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './auth/entities/User.entity';
+// import { Auth } from './auth/entities/auth.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [User, Auth],
-      synchronize: true,
-      poolSize: 5,
-      logging: false,
-      connectorPackage: 'mysql2',
-      extra: {
-        authPlugins: 'sha256_password',
-      },
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: +process.env.DB_PORT,
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [User, Auth],
+    //   synchronize: true,
+    //   poolSize: 5,
+    //   logging: false,
+    //   connectorPackage: 'mysql2',
+    //   extra: {
+    //     authPlugins: 'sha256_password',
+    //   },
+    // }),
     AuthModule,
   ],
   controllers: [AppController],
